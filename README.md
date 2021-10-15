788683409168c8e1c579feb8be1170ebb73ac974
lmmmowi <lmmmowi@163.com>
2021-10-15 10:31:20 +0800

    submit p5

---
+++ README.md
@@ -0,0 +1,9 @@
+# lintcode题目练习
+
+![Language](https://img.shields.io/badge/language-java-blue.svg)
+![Language](https://img.shields.io/badge/language-scala-red.svg)
+![Language](https://img.shields.io/badge/language-go-9cf.svg)
+
+| 题号 | 题目 | 解法 |
+| --- |:---:| :---:|
+| 5 | [第k大元素](https://www.lintcode.com/problem/5) | [java](https://github.com/lmmmowi/lintcode-practice/blob/master/src/main/java/com/lmmmowi/lintcode/p5/Solution.java) |
--- src/main/java/com/lmmmowi/lintcode/p5/Solution.java
+++ src/main/java/com/lmmmowi/lintcode/p5/Solution.java
@@ -3,7 +3,45 @@ package com.lmmmowi.lintcode.p5;
 /**
  * @Author: lmmmowi
  * @Date: 2021/10/15
- * @Description: 123. 12321312[433333]
+ * @Description: 5. 第k大元素[https://www.lintcode.com/problem/5]
  */
 public class Solution {
+
+    public int kthLargestElement(int k, int[] nums) {
+        qsort(k - 1, nums, 0, nums.length - 1);
+        return nums[k - 1];
+    }
+
+    private void qsort(int targetIndex, int[] nums, int fromIndex, int toIndex) {
+        if (fromIndex >= toIndex) {
+            return;
+        }
+
+        int low = fromIndex;
+        int high = toIndex + 1;
+        int pivot = nums[low];
+
+        while (true) {
+            while (nums[--high] < pivot && low < high) ;
+            if (low < high) {
+                nums[low] = nums[high];
+            } else {
+                break;
+            }
+
+            while (nums[++low] >= pivot && low < high) ;
+            if (low < high) {
+                nums[high] = nums[low];
+            } else {
+                break;
+            }
+        }
+
+        nums[low] = pivot;
+        if (targetIndex < low) {
+            qsort(targetIndex, nums, fromIndex, low - 1);
+        } else if (targetIndex > low) {
+            qsort(targetIndex, nums, low + 1, toIndex);
+        }
+    }
 }
