86b40cb46f11cb2c36fec6afed6e616cee25cef0
lmmmowi <lmmmowi@163.com>
2021-10-21 19:29:23 +0800

    update p184

--- src/main/java/com/lmmmowi/lintcode/p184/Solution.java
+++ src/main/java/com/lmmmowi/lintcode/p184/Solution.java
@@ -1,7 +1,6 @@
 package com.lmmmowi.lintcode.p184;
 
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.stream.Collectors;
 
 /**
@@ -12,32 +11,10 @@ import java.util.stream.Collectors;
 public class Solution {
 
     public String largestNumber(int[] nums) {
-        Comparator<String> comparator = (s1, s2) -> {
-            int l1 = s1.length();
-            int l2 = s2.length();
-            int len = Math.max(l1, l2);
-            for (int i = 0; i < len; i++) {
-                if (i < l1 && i < l2) {
-                    char c1 = s1.charAt(i);
-                    char c2 = s2.charAt(i);
-                    if (c1 > c2) {
-                        return -1;
-                    } else if (c1 < c2) {
-                        return 1;
-                    }
-                } else {
-                    int a = Integer.parseInt(s1 + s2);
-                    int b = Integer.parseInt(s2 + s1);
-                    return a > b ? -1 : 1;
-                }
-            }
-            return 0;
-        };
-
         String result = Arrays.stream(nums)
                 .boxed()
                 .map(String::valueOf)
-                .sorted(comparator)
+                .sorted((s1, s2) -> (s2 + s1).compareTo(s1 + s2))
                 .collect(Collectors.joining());
         return result.startsWith("0") ? "0" : result;
     }
